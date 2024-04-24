83855d48446e50f9093218191f4c0023fc0d5eed
milkessa <38739690+milkessa@users.noreply.github.com>
2024-04-23 20:26:49 -0400

    Update question20.cs

--- question20.cs
+++ question20.cs
@@ -1,1 +1,27 @@
+public class ValueTypeCollection<T> where T : struct
+{
+    private List<T> collection = new List<T>();
+
+    public void AddItem(T item)
+    {
+        collection.Add(item);
+    }
+
+    public T GetItem(int index)
+    {
+        if (index >= 0 && index < collection.Count)
+        {
+            return collection[index];
+        }
+        else
+        {
+            throw new IndexOutOfRangeException();
+        }
+    }
+
+    public List<T> GetSortedCollection()
+    {
+        return collection.OrderByDescending(x => x).ToList();
+    }
+}
 
